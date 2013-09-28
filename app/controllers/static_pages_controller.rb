9f25c10eda30db01d637c30c575484cccd284067
Van Michael <van.michael.nguyen@gmail.com>
2013-09-28 17:22:20 -0400

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+
+	def index
+	end
+
+	def admin_company_calendar
+	end
+
+	def company_calendar
+	end
+
+	def user_profile
+	end
+
+	def reciept
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppointmentApp::Application.config.secret_token = '2af5903674324842b8a3696d3f83fcc3c03890db04b003637e28aec3abba82812019f8ab0e3c1eb94c10f977310b864ac1a61d9fcb422b9c83e70a520d8aa927'
